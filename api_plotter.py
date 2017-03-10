e877ce69b0fc2f4e1d050c975ee261e68235b1d1
magicdoughnut <markgrahamdawson@gmail.com>
2017-03-10 12:03:01 +0000

    updated plotter

--- api_plotter.py
+++ api_plotter.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import datetime
+import numpy as np
+
 
 # Read our backups back in..
 df_companies = pd.read_pickle('companies.pkl')
@@ -14,5 +17,33 @@ df_fillings = pd.read_pickle('fillings.pkl')
 df_officer_change = df_fillings.query("category == 'officers'")
 df_joined =  pd.merge(df_officer_change, df_companies, on='company_number', how='left')[['title', 'action_date']]
 
-print df_joined.head()
-# np_thing = df_officer_change['title'].as_matrix()
+# print df_joined.head
+np_date_list = df_joined['action_date'].as_matrix()
+print len(np_date_list)
+
+# date_store = []
+# for date in np_date_list:
+# 	if str(date) != 'nan':
+# 		date_store.append(datetime.datetime.strptime(date, '%Y-%m-%d'))
+# 		# np.append(date_store,datetime.datetime.strptime(date, '%Y-%m-%d'))
+# 	else:
+# 		date_store.append('nan')
+# 		# np.append(date_store,'nan')
+
+# print(len(date_store))
+
+
+df_joined['action_date'] = pd.to_datetime(df_joined['action_date'])
+df_joined['zero'] = 0.0
+print df_joined['action_date']
+
+# exit()
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+py.sign_in('mdawson', 'rswfddp3bl')
+
+data = [go.Scatter(x=df_joined['action_date'],y=df_joined['zero'],mode='markers',name='Markers and Text',text=df_joined['title'],textposition='bottom')]
+py.iplot(data)
+
+
